63094e9bbfcba4dca03202bc08cc9a337f350ea0
Alexander Zinchenko <alexander@zinchenko.com>
2021-07-01 16:07:14 +0300

    update

--- root/app/end_backup.sh
+++ root/app/end_backup.sh
@@ -3,9 +3,9 @@
 
 dup_pid=$(ps | grep "duplicacy backup" | grep -v "grep" | awk '{print $1;}')
 
-if [[ ! -z ${dup_pid} ]]; then
+if [[ -n "${dup_pid}" ]]; then
     echo Stop current backup process with safe resume from current uploaded progress when next backup occurs
-    kill -2 $dup_pid
+    kill -2 "${dup_pid}"
 else
     echo No current backup process, nothing to stop
 fi
