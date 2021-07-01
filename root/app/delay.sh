c027ba7219eaf7804fc0958e0cac3303e89fbe4e
Alexander Zinchenko <alexander@zinchenko.com>
2021-07-01 15:37:59 +0300

    update

--- root/app/delay.sh
+++ root/app/delay.sh
@@ -5,14 +5,14 @@ source /app/common.sh
 
 log_file=$1
 
-if [[ -z ${log_file} ]]; then
+if [[ -z "${log_file}" ]]; then
     log_file=/dev/null
 fi
 
-if [[ ! -z ${JOB_RANDOM_DELAY} ]] && [[ $JOB_RANDOM_DELAY -ne 0 ]]; then
-    delay=$(((RANDOM % ($JOB_RANDOM_DELAY - 1)) + 1))
-    echo Backup delayed by $(converts $delay) | tee -a "${log_file}"
-    sleep $delay
+if [[ -n "${JOB_RANDOM_DELAY}" ]] && [[ "${JOB_RANDOM_DELAY}" -ne 0 ]]; then
+    delay=$(((RANDOM % (JOB_RANDOM_DELAY - 1)) + 1))
+    echo Backup delayed by "$(converts $delay)" | tee -a "${log_file}"
+    sleep "${delay}"
 fi
 
 exit 0
